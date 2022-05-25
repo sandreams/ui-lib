f620b74cea9f10d67c7d22e0dec1dc54ed4d746e
sanDream <fs517010856@gmail.com>
2022-05-25 21:18:07 +0800

    完成 SideNavigation 侧边栏导航

--- public/src/js/sidenavigation.js
+++ public/src/js/sidenavigation.js
@@ -6,19 +6,11 @@
         e.preventDefault()
         $(target).removeClass('on-view')
       })
+      $(this).on('click', '.modal-box', function (e) {
+        e.preventDefault()
+        $(target).removeClass('on-view')
+      })
     })
-    // $('.side-nav').on('click', '.nav-close',  function (e) {
-    //   //   e.preventDefault()
-    //   console.log('this :>> ', this)
-    //   console.log('e.currentTarget :>> ', e.currentTarget)
-    //   console.log('e.target :>> ', e.target)
-    // })
   }
-  //   const showLeft = () => {
-  //     console.log('left')
-  //   }
-  //   const showRight = () => {
-  //     console.log('right')
-  //   }
   bindEvents()
 })()
